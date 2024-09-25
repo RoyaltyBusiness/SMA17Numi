c8aca60f8c9c372581222c7fc83483e06ace9a69
Royalty <royaltybusiness1999@gmail.com>
2024-09-25 22:42:36 +0200

    Add files via upload

---
+++ MainPatcher.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx;
+using HarmonyLib;
+using Nautilus.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleFramework.VehicleTypes;
+using System.Collections;
+using VehicleFramework;
+using SMA17Numi;
+
+namespace SMA17Numi
+{
+    public static class Logger
+    {
+        public static void Log(string message)
+        {
+            UnityEngine.Debug.Log("[Numi]:" + message);
+        }
+        public static void Output(string msg)
+        {
+            BasicText message = new BasicText(500, 0);
+            message.ShowMessage(msg, 5);
+        }
+    }
+    [BepInPlugin("com.royalty.subnautica.Numi.mod", "Numi", "1.0.0")]
+    [BepInDependency("com.mikjaw.subnautica.vehicleframework.mod")]
+    [BepInDependency("com.snmodding.nautilus")]
+    [BepInDependency("com.Bobasaur.AircraftLib")]
+
+    public class MainPatcher : BaseUnityPlugin
+    {
+        public void Start()
+        {
+            var harmony = new Harmony("com.royalty.subnautica.Numi.mod");
+            harmony.PatchAll();
+            UWE.CoroutineHost.StartCoroutine(Numi.Register());
+        }
+
+
+    }
+}
---
+++ Numi.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using VehicleFramework;
+using VehicleFramework.VehicleParts;
+using VehicleFramework.VehicleTypes;
+using System.IO;
+using System.Reflection;
+using UnityEngine.U2D;
+using System.Collections;
+using UWE;
+using static Nautilus.Assets.PrefabTemplates.FabricatorTemplate;
+using UnityEngine.Assertions;
+using VehicleFramework.Assets;
+using AircraftLib;
+using VehicleFramework.Engines;
+
+namespace SMA17Numi
+{
+    public class Numi : PlaneVehicle
+    {
+        public static IEnumerator Register()
+        {
+            GetAssets();
+            Submersible Numi = assets.model.EnsureComponent<Numi>();
+            Numi.name = "Numi";
+            yield return CoroutineHost.StartCoroutine(VehicleRegistrar.RegisterVehicle(Numi));
+            yield break;
+        }
+        public static VehicleFramework.Assets.VehicleAssets assets;
+        public static void GetAssets()
+        {
+            assets = AssetBundleInterface.GetVehicleAssetsFromBundle("assets/numi", "numi_vehicle", "SpriteAtlas", "NumiPing", "NumiCrafter");
+        }
+        public override Atlas.Sprite CraftingSprite => VehicleFramework.Assets.SpriteHelper.GetSprite("assets/NumiCrafter.png");
+        public override Atlas.Sprite PingSprite => VehicleFramework.Assets.SpriteHelper.GetSprite("assets/NumiPing.png");
+        public override VehiclePilotSeat PilotSeat
+        {
+            get
+            {
+                VehicleFramework.VehicleParts.VehiclePilotSeat vps = new VehicleFramework.VehicleParts.VehiclePilotSeat();
+                Transform mainSeat = transform.Find("Pilotseat");
+                vps.Seat = mainSeat.gameObject;
+                vps.SitLocation = mainSeat.Find("SitLocation").gameObject;
+                vps.LeftHandLocation = mainSeat;
+                vps.RightHandLocation = mainSeat;
+                return vps;
+            }
+        }
+
+        public override List<VehicleHatchStruct> Hatches
+        {
+            get
+            {
+                var list = new List<VehicleFramework.VehicleParts.VehicleHatchStruct>();
+
+                foreach (Transform tr in CollisionModel.transform)
+                {
+                    VehicleFramework.VehicleParts.VehicleHatchStruct thisHatch = new VehicleFramework.VehicleParts.VehicleHatchStruct();
+                    thisHatch.Hatch = tr.gameObject;
+                    thisHatch.ExitLocation = transform.Find("Hatch/ExitPosition");
+                    thisHatch.SurfaceExitLocation = thisHatch.ExitLocation;
+                    list.Add(thisHatch);
+                }
+
+                return list;
+            }
+        }
+
+        public override GameObject VehicleModel
+        {
+            get
+            {
+                return assets.model;
+            }
+        }
+
+        public override GameObject CollisionModel
+        {
+            get
+            {
+                return transform.Find("Collider").gameObject;
+            }
+        }
+
+        public override GameObject StorageRootObject
+        {
+            get
+            {
+                return transform.Find("StorageRoot").gameObject;
+            }
+        }
+
+        public override GameObject ModulesRootObject
+        {
+            get
+            {
+                return transform.Find("ModulesRoot").gameObject;
+            }
+        }
+        public override List<VehicleUpgrades> Upgrades
+        {
+            get
+            {
+                var list = new List<VehicleFramework.VehicleParts.VehicleUpgrades>();
+                VehicleFramework.VehicleParts.VehicleUpgrades vu = new VehicleFramework.VehicleParts.VehicleUpgrades();
+                vu.Interface = transform.Find("Upgrades").gameObject;
+                vu.Flap = vu.Interface;
+                list.Add(vu);
+                return list;
+            }
+        }
+
+        public override List<VehicleBattery> Batteries
+        {
+            get
+            {
+                var list = new List<VehicleFramework.VehicleParts.VehicleBattery>();
+
+                VehicleFramework.VehicleParts.VehicleBattery vb1 = new VehicleFramework.VehicleParts.VehicleBattery();
+                vb1.BatterySlot = transform.Find("Batteries/Battery1").gameObject;
+                list.Add(vb1);
+
+                VehicleFramework.VehicleParts.VehicleBattery vb2 = new VehicleFramework.VehicleParts.VehicleBattery();
+                vb2.BatterySlot = transform.Find("Batteries/Battery2").gameObject;
+                list.Add(vb2);
+
+                return list;
+            }
+        }
+
+        public override List<VehicleFloodLight> HeadLights
+        {
+            get
+            {
+                var list = new List<VehicleFramework.VehicleParts.VehicleFloodLight>();
+
+                VehicleFramework.VehicleParts.VehicleFloodLight Front = new VehicleFramework.VehicleParts.VehicleFloodLight
+                {
+                    Light = transform.Find("lights_parent/headlights/Front").gameObject,
+                    Angle = 70,
+                    Color = Color.white,
+                    Intensity = 1.3f,
+                    Range = 90f
+                };
+                list.Add(Front);
+                return list;
+            }
+        }
+
+
+        public override List<GameObject> WaterClipProxies
+        {
+            get
+            {
+                var list = new List<GameObject>();
+                foreach (Transform child in transform.Find("WaterClipProxies"))
+                {
+                    list.Add(child.gameObject);
+                }
+                return list;
+            }
+        }
+
+        public override List<GameObject> CanopyWindows
+        {
+            get
+            {
+                var list = new List<GameObject>();
+                list.Add(transform.Find("Model/Canopy").gameObject);
+                return list;
+            }
+        }
+
+        public override GameObject BoundingBox
+        {
+            get
+            {
+                return transform.Find("BoundingBox").gameObject;
+            }
+        }
+
+        public override Dictionary<TechType, int> Recipe
+        {
+            get
+            {
+                Dictionary<TechType, int> recipe = new Dictionary<TechType, int>();
+                recipe.Add(TechType.PlasteelIngot, 2);
+                recipe.Add(TechType.PowerCell, 2);
+                recipe.Add(TechType.EnameledGlass, 1);
+                recipe.Add(TechType.AdvancedWiringKit, 2);
+                return recipe;
+            }
+        }
+        public override string vehicleDefaultName
+        {
+            get
+            {
+                Language main = Language.main;
+                bool flag = !(main != null);
+                string result;
+                if (flag)
+                {
+                    result = "SMA-17 Numi";
+                }
+                else
+                {
+                    result = main.Get("SMA-17 Numi");
+                }
+                return result;
+            }
+        }
+
+        public override string Description => "Super Manuverability Aircraft 17";
+
+        public override string EncyclopediaEntry => "Warning: Following vehicle is not designed nor produced by Alterra.\nIts effectiveness and specifications might not be at the Alterra standards and may pose danger to its user.\nThis technology cannot be fully read due to the difference in Alterra and Verdent corporation’s blueprint systems, but we will try to interpret this blueprint's features as closely as possible and for enhanced user experience it will use Alterra hud.\nAll its weapon systems will be completely removed, due to the structural integration of its gun the aircraft will retain its gun but will be completely disabled and locked.\nFollowing user to-read information is directly provided by Verdent Corporation.\n\nSuperManeuverability Aircraft-17 is a military aircraft produced by Verdent corporation.\nIt is the first aircraft to use air-breathing electric jet engines. These engines use the air passing through and rapidly ionize it, providing a large amount of thrust.\n\nIt possesses an 8-barrel 25mm gun [Weapon was disabled]\nTypically possesses 8 hardpoints [Hardpoints were completely removed]\n\nIt uses traditional gauges and instruments in the cockpit, providing accurate data about the surroundings [those systems were disabled due to integration to the Alterra hud]\n\nIt is powered by two power cells due to the substantial efficiency of the engines."
+;
+
+        public override int BaseCrushDepth => 1;
+
+        public override int CrushDepthUpgrade1 => 10;
+
+        public override int CrushDepthUpgrade2 => 50;
+
+        public override int CrushDepthUpgrade3 => 100;
+
+        public override int MaxHealth => 3400;
+
+        public override int Mass => 5000;
+        public override int NumModules => 6;
+        public override ModVehicleEngine Engine
+        {
+            get
+            {
+                return Radical.EnsureComponent<PlaneRCAgileEngine>(base.gameObject);
+            }
+        }
+        protected float _maxSpeed = 200f;
+        public override float maxSpeed
+        {
+            get
+            {
+                return _maxSpeed;
+            }
+            set
+            {
+                _maxSpeed = value;
+            }
+        }
+
+        protected float _takeoffSpeed = 35f;
+        public override float takeoffSpeed
+        {
+            get
+            {
+                return _takeoffSpeed;
+            }
+            set
+            {
+                _takeoffSpeed = value;
+            }
+        }
+        public override void Update()
+        {
+            base.Update();
+            AircraftLib.FlightManager.CheckLandingGear(this);
+        }
+    }
+}
